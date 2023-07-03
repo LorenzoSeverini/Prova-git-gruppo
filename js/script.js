2bb0271fdfaf456ea02e02271210d9f5b0d05809
Edy Galdiolo <edygaldy302@gmail.com>
2023-07-03 17:33:18 +0200

    feat(main): aggiunti links e cambiato ordine feat(js): aggiunto un hover e modificata la funzione setTimeout feat(style): aggiunte classi

--- js/script.js
+++ js/script.js
@@ -9,5 +9,20 @@ btn.addEventListener('click', function () {
     // show an image after 3 seconds
     setTimeout(function () {
         document.getElementById('img').classList.remove('hide');
+        document.getElementById('clear-js').classList.add('hide');
     }, 1000);
 });
+
+
+// Image over for the footer
+const hover = document.getElementsByClassName('image-hover');
+var remove = document.getElementsByClassName('footer-absolute');
+for (let i = 0; i < hover.length; i++) {
+    hover[i].addEventListener('mouseover', function() {
+        remove[i].classList.remove('hide');
+    })
+
+    hover[i].addEventListener('mouseleave', function() {
+        remove[i].classList.add('hide');
+    })
+}
